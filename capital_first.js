a6155ab1c82d39a18c845c1a586c5fd3e9ba5eef
dreyhiflden <dreyhiflden@gmail.com>
2019-01-30 15:47:32 +0200

    Add first sub-task (sort string)

--- capital_first.js
+++ capital_first.js
@@ -1,5 +1,16 @@
-function capitalsFirst(str){
+function capitalsFirst(str) {
+  let checkCase = char => char === char.toUpperCase() ? 1 : 2;
 
+  return str
+    .split(' ')
+    .sort((a, b) => checkCase(a[0]) - checkCase(b[0]))
+    .join(' ');
 }
 
-// Test.assertEquals(capitalsFirst("hey You, Sort me Already!"), "You, Sort Already! hey me")
+const sortedString = capitalsFirst('hey You, Sort me Already!');
+const expectedString = 'You, Sort Already! hey me';
+
+console.assert(
+  sortedString === expectedString,
+  `Oh no! ${sortedString} not equals ${expectedString}`,
+);
